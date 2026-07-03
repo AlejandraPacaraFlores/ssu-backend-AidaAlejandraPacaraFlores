2342e4536fe97bd6a00d1f5df3d04fb89bfca2fb
Aida Alejadra Pacara Flores <af75781459@gmail.com>
2026-07-03 10:56:34 -0400

    feat:Aguegando la tabla reservas

---
+++ src/main/java/com/ssu/backend/repository/ReservaRepository.java
@@ -0,0 +1,9 @@
+package com.ssu.backend.repository;
+
+import com.ssu.backend.entity.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+}
